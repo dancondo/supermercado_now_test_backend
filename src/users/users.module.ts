d27c65e7b0da0bf6a3a750a360857f410ecdfe3b
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-02-04 20:22:20 -0300

    updates users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './users.schema';
+import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  providers: [UsersService]
+    imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
+    controllers: [UsersController],
+    providers: [UsersService],
+    exports: [UsersService]
 })
 export class UsersModule {}
