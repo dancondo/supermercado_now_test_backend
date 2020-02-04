1694048af222e55c3a6f553233aa266c15988840
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-02-04 20:16:18 -0300

    creates create user dto

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreateUserDto {
+  
+  @ApiProperty()
+  readonly email: string;
+
+  @ApiProperty()
+  readonly password: string;
+
+  @ApiProperty()
+  readonly firstName: string;
+
+  @ApiProperty()
+  readonly lastName: string;
+}
