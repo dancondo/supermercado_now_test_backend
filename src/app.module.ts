b2555170ccda9aa2bb2e19ec821d95f16999b3fc
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-02-04 20:22:02 -0300

    remove files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
@@ -13,7 +11,5 @@ import { UsersModule } from './users/users.module';
     AuthModule,
     UsersModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule { }
