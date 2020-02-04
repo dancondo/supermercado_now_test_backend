59226eeaf092521db2ae000919b29b04910f80cd
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-02-04 19:51:11 -0300

    uses passport in auth module and do the registration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: async () => {
+        return {
+          secret: process.env.APP_SECRET,
+          signOptions: {
+            expiresIn: process.env.EXPIRES_IN || '1h'
+          }
+        }
+      },
+    })
+  ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
+  exports: [AuthService]
 })
 export class AuthModule {}
