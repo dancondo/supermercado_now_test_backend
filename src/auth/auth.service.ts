55f48fdeee003e230f95e28d0077e50cd07e50c9
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-02-04 19:50:53 -0300

    Creates auth service with vailidation functions

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import { User } from '../users/users.model';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService
+  ) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+    if (user && user.password === password) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async signJwt(user: User) {
+    const payload = { id: user._id, firstName: user.firstName }
+    return this.jwtService.sign(payload);
+  }
+
+  async findByPayload(payload: any) {
+    return this.usersService.findById(payload.id);
+  }
+
+}
