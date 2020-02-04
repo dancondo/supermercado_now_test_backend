f7b26332ddd22a0b0b58ad973c9ec4213e050287
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-02-04 19:51:43 -0300

    creates users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './users.model';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+  constructor(@InjectModel('User') private readonly users: Model<User>) {}
+
+  async findOne(email: string): Promise<User | undefined> {
+    return this.users.findOne({ email: email });
+  }
+
+  async findById(id): Promise<User> {
+    return this.users.findById(id).exec();
+  }
+}
