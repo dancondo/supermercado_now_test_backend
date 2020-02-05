c7282f1e302a0e7dc7e1f378d6692a307eb70460
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-02-05 19:01:30 -0300

    imports animes module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { AnimesController } from './animes/animes.controller';
+import { AnimesService } from './animes/animes.service';
+import { AnimesModule } from './animes/animes.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot('mongodb://localhost/supermercado-now'),
     AuthModule,
-    UsersModule
+    UsersModule,
+    AnimesModule
   ],
+  controllers: [AnimesController],
+  providers: [AnimesService],
 })
 export class AppModule { }
