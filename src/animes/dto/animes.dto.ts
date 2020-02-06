7c6028330a12e8b7b360e62cbf1e33e03200e11e
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-02-06 17:13:03 -0300

    add favorite field to animes controller

--- src/animes/dto/animes.dto.ts
+++ src/animes/dto/animes.dto.ts
@@ -14,11 +14,15 @@ export class AnimeDto {
   @ApiProperty()
   readonly synopsis: string;
 
-  constructor({ mal_id, image_url, title, synopsis }) {
+  @ApiProperty()
+  readonly favorite: boolean
+
+  constructor({ mal_id, image_url, title, synopsis }, ids?: Array<string>) {
     this.id = mal_id;
     this.imageUrl = image_url;
     this.title = title;
     this.synopsis = synopsis;
+    this.favorite = ids ? ids.includes(mal_id) : false
   }
 
 }
