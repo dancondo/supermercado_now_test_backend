5b468624682ef84db6a7294d401184addc01a7cd
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-02-06 05:47:06 -0300

    imports image upload service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './users.schema';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { ImageUploadService } from 'src/images/images.service';
 
 @Module({
     imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
     controllers: [UsersController],
-    providers: [UsersService],
+    providers: [UsersService, ImageUploadService],
     exports: [UsersService]
 })
 export class UsersModule {}
