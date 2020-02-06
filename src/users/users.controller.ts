2df775a2cd83907a46114aa160092ecb30238189
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2020-02-06 05:46:42 -0300

    creates update user and adds image uploader

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,11 @@
-import { Controller, Get, HttpStatus, UseGuards, Put, Request, Body } from '@nestjs/common';
+import { Controller, Get, HttpStatus, UseGuards, Put, Request, Body, UseInterceptors, UploadedFile, Res, Next } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 import { UserDto } from './dto/user.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { ImageUploadService } from 'src/images/images.service';
+import { UpdateUserDto } from './dto/update-user-dto';
 
 @ApiTags('User')
 @ApiBearerAuth()
@@ -11,7 +14,8 @@ import { AuthGuard } from '@nestjs/passport';
 export class UsersController {
 
   constructor(
-    private readonly userService: UsersService
+    private readonly userService: UsersService,
+    private readonly imageService: ImageUploadService
   ) { }
 
   @ApiOperation({
@@ -42,4 +46,27 @@ export class UsersController {
   ): Promise<UserDto> {
     return new UserDto(req.user);
   }
+
+  @ApiOperation({
+    description: 'Updates an user profile'
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Successfully updated an user'
+  })
+  @Put('me')
+  @UseInterceptors(FileInterceptor('image'))
+  public async update(
+    @Request() req,
+    @Body() body,
+    @UploadedFile() image,
+    @Body() user: UpdateUserDto
+  ): Promise<UserDto> {
+    console.log('image', image);
+    if (image) {
+      user.imageUrl = await this.imageService.upload(image);
+    }
+    return new UserDto(await this.userService.update(req.user._id, user));
+  }
+
 }
